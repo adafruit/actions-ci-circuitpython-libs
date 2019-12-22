3b63cdc3499394b7a157442009b947de85e9034a
dherrada <33632497+dherrada@users.noreply.github.com>
2019-12-22 13:12:21 -0500

    Added install deps from build.yml and release.yml

--- install.sh
+++ install.sh
@@ -12,5 +12,9 @@ if [[ $# -eq 0 ]] ; then
   GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
 fi
 
-# Install libusb
+# Install dependencies
 sudo apt-get install libudev-dev libusb-1.0
+sudo apt-get install -y gettext
+pip install -r requirements.txt
+pip install circuitpython-build-tools Sphinx sphinx-rtd-theme
+pip install --force-reinstall pylint==1.9.2
