52fe4ee703f3a96a89e5943a08b499e69bc1202a
Tekktrik <tekktrik@gmail.com>
2023-07-11 20:36:59 -0400

    Added script for printing out pre-commit hook versions

--- install.sh
+++ install.sh
@@ -16,12 +16,25 @@ if [[ $# -eq 0 ]] ; then
   GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
 fi
 
-# Install dependencies
+# Install package dependencies
 sudo apt-get update
 sudo apt-get install libudev-dev libusb-1.0
 sudo apt-get install -y gettext
+
+# Install dependencies for these scripts
+if test -d "actions-ci"; then
+  pip install -r actions-ci/requirements.txt
+else
+  echo "Directory 'actions-ci' does not exist!"
+  echo "CI expects the 'actions-ci-circuitpython-libs' tools being checked out to said directory."
+  exit 1
+fi
+
+# Install dependencies for the library
 pip install -r requirements.txt
 if test -f "optional_requirements.txt"; then
     pip install -r optional_requirements.txt
 fi
+
+# Install build tool
 pip install circuitpython-build-tools
---
+++ print_precommit_versions.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Alec Delaney, for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Script for printing the versions of the contents of .pre-commit-config.yaml
+
+Author(s): Alec Delaney, for Adafruit Industries
+"""
+
+from typing import TypedDict
+import yaml
+
+
+class PreCommitRepo(TypedDict):
+    """Typed dictionary structure of a pre-commit hook"""
+
+    repo: str
+    rev: str
+    hooks: list[dict[str, str]]
+
+
+with open(".pre-commit-config.yaml", mode="r", encoding="utf-8") as yamlfile:
+    repos: list[PreCommitRepo] = yaml.safe_load(yamlfile)["repos"]
+
+print("Using the following pre-commit hook versions:")
+for repo in repos:
+    print(f' * {repo["repo"]} @ {repo["rev"]}')
